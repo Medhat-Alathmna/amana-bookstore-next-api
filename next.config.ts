44b9dd8cea72e5211cdc7394cc2a7686bf8d3b2d
Medhat-Alathmna <medhatalathmna@gmail.com>
2025-10-03 19:45:34 +0300

    Redirect root path to /api/books
    Added a redirect from '/' to '/api/books' in next.config.ts and implemented a client-side redirect in app/page.tsx to ensure users are directed to the books API on initial load.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/api/books',
+        permanent: true,
+      },
+    ]
+  },
 };
 
 export default nextConfig;
